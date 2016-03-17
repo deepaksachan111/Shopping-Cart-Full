36a1211397744403c51286a0ceef8e51a945f8d3
deepak <deepaksachan8@gmail.com>
2016-03-17 17:20:49 +0530

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/dreamdom/tutorials/shoppingcart/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dreamdom.tutorials.shoppingcart;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.dreamdom.tutorials.shoppingcart";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
